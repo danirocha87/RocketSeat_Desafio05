7c464ded533872e6c42b1da9b68cca86dd2c118c
daniela oliveira <danioliver_17@hotmail.com>
2023-05-01 14:42:08 -0300

    DESAFIO 05

--- js/sounds.js
+++ js/sounds.js
@@ -1,10 +1,7 @@
 export default function () {
   const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
-  const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
-  const bgRain = new Audio("https://github.com/4llves/myAssets/blob/main/rain.mp3?raw=true")
-  const bgForest = new Audio("https://github.com/4llves/myAssets/blob/main/florest.mp3?raw=true")
-  const bgFire = new Audio("https://github.com/4llves/myAssets/blob/main/fire.mp3?raw=true")
-  const bgCoffeeShop = new Audio("https://github.com/4llves/myAssets/blob/main/coffeeShop.mp3?raw=true")
+  const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true");
+
 
 
   function pressButton() {
